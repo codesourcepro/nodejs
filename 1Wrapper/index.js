f85a10a07d2872358d4ee9510c79683e75425bc1
Pramod Chaudhary <c10.promod@gmail.com>
2023-07-28 00:55:23 +0400

    1. Wrapper Module in NODEJS

---
+++ 1Wrapper/index.js
@@ -0,0 +1,6 @@
+console.log("Wrapper Module Details")
+
+// (function (exports, require, module, __filename, __dirname) {
+//     console.log("Exact function of wrapper")
+// });
+console.log(require);
